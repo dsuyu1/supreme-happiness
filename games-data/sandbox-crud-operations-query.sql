e1d01b8ea916e8d8cd5550ce1a03a2f9b83901b7
dsuyu1 <damian.villarreal01@utrgv.edu>
2022-11-22 13:21:55 -0600

    games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int16_value, bool_value, string_value) VALUES (2005, 1, 'i want a dog')
+
+--UPDATE sandbox SET int16_value = 2022 WHERE id = 72;
+
+--DELETE FROM sandbox WHERE id = 72; 
+
+SELECT * FROM sandbox; 
